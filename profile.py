f74cc88eda15a35db1a080a10d16e05137822b02
ryanfio15 <ryanfio15@gmail.com>
2026-03-17 20:25:13 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,16 +1,15 @@
 import geni.portal as portal
 import geni.rspec.pg as rspec
 
-# Create a Request object to start building the RSpec.
 request = portal.context.makeRequestRSpec()
-# Create a XenVM
+
 node = request.XenVM("node")
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
 node.routable_control_ip = "true"
 
-node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
-node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y apache2"))
-node.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status apache2'))
+node.addService(rspec.Execute(
+    shell="/bin/bash",
+    command="sudo bash /local/repository/install_docker.sh"
+))
 
-# Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
